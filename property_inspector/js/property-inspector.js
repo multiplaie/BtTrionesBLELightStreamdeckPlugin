ba6ad583a25c1a2b8c6242b4243cb5618a53852f
multiplaie <multiplaie@protonmail.com>
2021-04-23 12:14:20 +0200

    adapt front side

--- property_inspector/js/property-inspector.js
+++ property_inspector/js/property-inspector.js
@@ -1,61 +1,83 @@
 ﻿// global websocket, used to communicate from/to Stream Deck software
 // as well as some info about our plugin, as sent by Stream Deck software 
 var websocket = null,
-  uuid = null,
-  inInfo = null,
-  actionInfo = {},
-  settingsModel = {
-	Counter: 0
-  };
+	uuid = null,
+	inInfo = null,
+	actionInfo = {},
+	settingsModel = {
+		LightMacAddr: 'xx:xx:xx:xx:xx:xx',
+		Switch: '000000',
+		Color: '000000'
+	};
 
 function connectElgatoStreamDeckSocket(inPort, inUUID, inRegisterEvent, inInfo, inActionInfo) {
-  uuid = inUUID;
-  actionInfo = JSON.parse(inActionInfo);
-  inInfo = JSON.parse(inInfo);
-  websocket = new WebSocket('ws://localhost:' + inPort);
+	uuid = inUUID;
+	actionInfo = JSON.parse(inActionInfo);
+	inInfo = JSON.parse(inInfo);
+	websocket = new WebSocket('ws://localhost:' + inPort);
 
-  //initialize values
-  if (actionInfo.payload.settings.settingsModel) {
-	settingsModel.Counter = actionInfo.payload.settings.settingsModel.Counter;
-  }
+	//initialize values
+	if (actionInfo.payload.settings.settingsModel) {
+		settingsModel.LightMacAddr = actionInfo.payload.settings.settingsModel.LightMacAddr;
+		settingsModel.Switch = actionInfo.payload.settings.settingsModel.Switch;
+		settingsModel.Color = actionInfo.payload.settings.settingsModel.Color;
+		settingsModel.Name = actionInfo.payload.settings.settingsModel.Name;
 
-  document.getElementById('txtCounterValue').value = settingsModel.Counter;
-
-  websocket.onopen = function () {
-	var json = { event: inRegisterEvent, uuid: inUUID };
-	// register property inspector to Stream Deck
-	websocket.send(JSON.stringify(json));
-
-  };
-
-  websocket.onmessage = function (evt) {
-	// Received message from Stream Deck
-	var jsonObj = JSON.parse(evt.data);
-	var sdEvent = jsonObj['event'];
-	switch (sdEvent) {
-	  case "didReceiveSettings":
-		if (jsonObj.payload.settings.settingsModel.Counter) {
-		  settingsModel.Counter = jsonObj.payload.settings.settingsModel.Counter;
-		  document.getElementById('txtCounterValue').value = settingsModel.Counter;
-		}
-		break;
-	  default:
-		break;
 	}
-  };
+	websocket.onopen = function () {
+		console.log(settingsModel);
+		setSelectField('light', settingsModel.LightMacAddr);
+		setSelectField('switch', settingsModel.Switch);
+		document.getElementById("color").value = '#' + settingsModel.Color;
+
+
+		var json = { event: inRegisterEvent, uuid: inUUID };
+		// register property inspector to Stream Deck
+		websocket.send(JSON.stringify(json));
+
+	};
+
+	websocket.onmessage = function (evt) {
+		// Received message from Stream Deck
+		var jsonObj = JSON.parse(evt.data);
+		var sdEvent = jsonObj['event'];
+		switch (sdEvent) {
+			case "didReceiveSettings":
+
+				break;
+			default:
+				break;
+		}
+	};
+	function setSelectField(id, value) {
+		var allInputs = document.getElementById(id);
+		for (var x = 0; x < allInputs.length; x++)
+			if (allInputs[x].value == value) {
+				allInputs[x].setAttribute('selected', 'selected');
+				console.log(allInputs[x]);
+			}
+
+	}
 }
 
 const setSettings = (value, param) => {
-  if (websocket) {
-	settingsModel[param] = value;
-	var json = {
-	  "event": "setSettings",
-	  "context": uuid,
-	  "payload": {
-		"settingsModel": settingsModel
-	  }
-	};
-	websocket.send(JSON.stringify(json));
-  }
+	console.log(param + "=" + value);
+	if (websocket) {
+
+		if (param == 'Color')
+			value = value.substring(1);
+
+		settingsModel[param] = value;
+		var json = {
+			"event": "setSettings",
+			"context": uuid,
+			"payload": {
+				"settingsModel": settingsModel
+			}
+		};
+
+		websocket.send(JSON.stringify(json));
+	}
+
 };
 
